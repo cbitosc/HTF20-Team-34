a5858bcc0d1f5234b244004176de6e2251796038
tariqahmed1912 <tariqahmed1912@gmail.com>
2020-10-30 20:49:00 +0530

    Views changed

--- Leave_Notifier/app1/views.py
+++ Leave_Notifier/app1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import logout, authenticate, login
+import pandas as pd
+from django.contrib.auth.models import User
+from django.contrib import messages
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Create your views here.
+def login(request):
+    return HttpResponse("Good work boi")
