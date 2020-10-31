f81b68c0228e585594c844e7e1439c12aa8cfaba
tariqahmed1912 <56790218+tariqahmed1912@users.noreply.github.com>
2020-10-31 07:27:16 +0530

    Update settings.py

--- Leave_Notifier/Leave_Notifier/settings.py
+++ Leave_Notifier/Leave_Notifier/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "tariqahmed1912@gmail.com"
-EMAIL_HOST_PASSWORD = 'torikotoriko'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = '587'
